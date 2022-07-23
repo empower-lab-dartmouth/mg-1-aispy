8c62d4c5c2053f2aa4a577368b914a3ddc5f422f
actbee <rainman511@outlook.com>
2022-07-22 22:24:39 -0400

    updated

--- src/store/atoms.tsx
+++ src/store/atoms.tsx
@@ -1,6 +1,7 @@
 import {atom, selector} from "recoil";
+import {image} from "../models/image";
 
-export const currentimage = atom({
+export const currentimage = atom<image>({
     key: "current-image",
     default: {
         "lable" : "",
